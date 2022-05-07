ca14b3d03035ced1305c31e29c31623dd4aae9b2
chivalrous <tony@starkindustries>
2022-05-07 14:10:40 +0530

    map api integration

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { EmployeeServiceService } from '../employee-service.service';
+import * as Leaflet from 'leaflet';
+import { Coordinates } from '../interfaces/coordinate.interface';
 
 @Component({
   selector: 'app-employee-details',
@@ -9,6 +11,14 @@ import { EmployeeServiceService } from '../employee-service.service';
 })
 export class EmployeeDetailsComponent implements OnInit {
   response: any;
+  marker = new Leaflet.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
+    iconSize: [32, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowSize: [41, 41]
+  });
 
   constructor(private activatedRoute: ActivatedRoute, private employeeService: EmployeeServiceService) { }
 
@@ -18,8 +28,33 @@ export class EmployeeDetailsComponent implements OnInit {
       this.employeeService.getEmployee(params.get('uuid')!).subscribe((response: any) => {
         this.response = response;
         console.log(this.response)
+        if(this.response?.results[0]?.coordinates != undefined){
+          console.log(this.response?.results[0]?.coordinates)
+        this.loadMap(this.response?.results[0]?.coordinates)
+        }
       })
     })
+    
   }
+
+  private loadMap(coordinate: Coordinates): void {
+    console.log("inside map function")
+    const map = Leaflet.map('map', {
+      center: [coordinate.latitude, coordinate.longitude],
+      zoom: 8
+    });
+    const mainLayer = Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      tileSize: 512,
+      zoomOffset: -1,
+      minZoom: 1,
+      maxZoom:30,
+      crossOrigin: true,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    mainLayer.addTo(map);
+    const marker = Leaflet.marker([coordinate.latitude, coordinate.longitude], { icon: this.marker });
+    marker.addTo(map).bindPopup(`${this.response.results[0].firstName}'s Location`).openPopup();
+  }
+
 
 }
